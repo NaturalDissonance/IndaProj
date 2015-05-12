659c4b58b4dd07dad23088b34a42a78ea7d7af5d
NaturalDissonance <Mr.usefull@hotmail.se>
2015-05-12 15:30:10 +0200

    Lite Arbete

---
+++ SpaceInv/src/Mains/MainGameLoop.java
@@ -0,0 +1,48 @@
+package Mains;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class MainGameLoop extends BasicGame {
+
+	public MainGameLoop(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		MainGameLoop game= new MainGameLoop("Journey of the epic space man");
+		try {
+	          AppGameContainer container = new AppGameContainer(game);
+	          container.start();
+	     } catch (SlickException e) {
+	          // TODO Auto-generated catch block
+	          e.printStackTrace();
+	     }
+
+	}
+
+}
